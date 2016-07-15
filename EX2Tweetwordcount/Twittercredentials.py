d4e2bdff9c633a3537e179581595a020ace054cd
ganeshsberkeley <ganeshs_sd@yahoo.com>
2016-07-15 13:22:34 -0500

    Update Twittercredentials.py

--- EX2Tweetwordcount/Twittercredentials.py
+++ EX2Tweetwordcount/Twittercredentials.py
@@ -1,16 +1,16 @@
 import tweepy
 
-consumer_key = "YourConsumerkey";
+consumer_key = "	Xi136MPzusLFDnk4uETJnV7VD";
 #eg: consumer_key = "YisfFjiodKtojtUvW4MSEcPm";
 
 
-consumer_secret = "YourConsumerSecret";
+consumer_secret = "e4TigozuPdDVIIJnH38pUHUmi9wJORml5jm71rkYqde274hFSi";
 #eg: consumer_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token = "YourAcessToken";
+access_token = "754010347967614977-BVru3rQngbCMOX6yWu39lhzEFBfovaZ";
 #eg: access_token = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token_secret = "YourTokenSecret";
+access_token_secret = "LEp1MEp3wuR8NCF1OTd9GdJVIKU7XIemnvj2noi5eAgOB";
 #eg: access_token_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
 
