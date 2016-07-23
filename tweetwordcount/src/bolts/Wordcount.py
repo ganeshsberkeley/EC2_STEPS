9e9f0ec308868fa54621136bc3c7425a9b333698
root <root@ip-172-31-12-155.ec2.internal>
2016-07-23 20:29:23 +0000

    Update

--- tweetwordcount/src/bolts/Wordcount.py
+++ tweetwordcount/src/bolts/Wordcount.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from collections import Counter
 from streamparse.bolt import Bolt
-from psycopg import psycopg2
 
 
 class WordCounter(Bolt):
